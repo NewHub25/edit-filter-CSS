c055537d3af2adef57c480eea478746be0c30dc7
Terry <master_correo144@hotmail.com>
2023-07-09 01:55:47 -0500

    Se mejoro el index js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,40 @@
 let valueToCss = "none";
-const screen = document.querySelector(".filters__header");
-const inputText = document.getElementById("colorText");
-const colorPaint = document.getElementById("colorPaint");
-const copy = document.getElementById("copy");
-const filterToCopy = document.getElementById("filterToCopy");
 
-document.querySelector(".filters__nav").addEventListener("change", (e) => {
-  const { target } = e;
-  if (target.tagName === "INPUT") {
-    document.documentElement.style.setProperty("--output", target.value);
-    if (target.id !== "colorText") {
-      inputText.value = target.value;
-    } else {
-      colorPaint.value = target.value;
+addEventListener("load", function () {
+  const screen = document.querySelector(".filters__header");
+  const inputText = document.getElementById("colorText");
+  const colorPaint = document.getElementById("colorPaint");
+  const copy = document.getElementById("copy");
+  const textarea = document.getElementById("textarea");
+
+  document.querySelector(".filters__nav").addEventListener("change", (e) => {
+    const { target } = e;
+    if (target.tagName === "INPUT") {
+      document.documentElement.style.setProperty("--output", target.value);
+      if (target.id !== "colorText") {
+        inputText.value = target.value;
+      } else {
+        colorPaint.value = target.value;
+      }
     }
-  }
-});
+  });
 
-document.querySelector(".filters__section").addEventListener("change", (e) => {
-  const { target } = e;
-  if (target.tagName === "INPUT") {
-    filterToCopy.value = "filter: " + buildFilter(target, screen);
-    target.parentElement.querySelector(".filters__span_data").innerHTML =
-      target.value + target.dataset.unit;
-  }
+  document
+    .querySelector(".filters__section")
+    .addEventListener("change", (e) => {
+      const { target } = e;
+      if (target.tagName === "INPUT") {
+        textarea.value = "filter: " + buildFilter(target, screen);
+        target.parentElement.querySelector(".filters__span_data").innerHTML =
+          target.value + target.dataset.unit;
+      }
+    });
+
+  copy.addEventListener("click", COPY__HTML);
+
+  document
+    .querySelector(".filters")
+    .addEventListener("submit", (e) => e.preventDefault());
 });
 function buildFilter(input, _screen) {
   if (valueToCss === "none") {
@@ -40,7 +51,7 @@ function buildFilter(input, _screen) {
   } else {
     valueToCss = valueToCss.replace(
       RegExp(`(${filter})\\(.+?\\)`),
-      value === defaultValue? '': `$1(${value + unit})`
+      value === defaultValue ? "" : `$1(${value + unit})`
     );
   }
   console.clear();
@@ -49,9 +60,9 @@ function buildFilter(input, _screen) {
   return valueToCss;
 }
 
-copy.onclick = function (e) {
+function COPY__HTML() {
   navigator.clipboard
-    .writeText(filterToCopy.value + ';')
+    .writeText(textarea.value + ";")
     .then(() => {
       this.classList.add("copied");
       setTimeout(() => this.classList.remove("copied"), 1000);
@@ -60,4 +71,4 @@ copy.onclick = function (e) {
     .catch(function (error) {
       console.error("Error al copiar el texto: " + error);
     });
-};
+}
